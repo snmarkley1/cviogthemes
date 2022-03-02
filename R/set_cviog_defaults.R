9823e210e4a1e64cd758f36aa9ac74c8b0ab5c7c
Scott Markley <sm62763@uga.edu>
2022-03-02 16:25:27 -0500

    update

--- R/set_cviog_defaults.R
+++ R/set_cviog_defaults.R
@@ -1,6 +1,6 @@
-#' The Urban Institute [ggplot2] theme
+#' The University of Georgia [ggplot2] theme
 #'
-#' \code{set_urbn_defaults} provides a [ggplot2] theme formatted according to the
+#' \code{set_cviog_defaults} provides a [ggplot2] theme formatted according to the
 #' Urban Institute style guide, with sensible defaults.
 #'
 #' @param style The default theme style for the R session. "print" or "map".
@@ -11,32 +11,32 @@
 #' relative to this value.
 #' @param base_rect_size The base rect size for the theme. All rect sizes are
 #' relative to this value.
-#' @param scale For `theme_urbn_map()`. Should the legend theme be continuous or discrete?
+#' @param scale For `theme_cviog_map()`. Should the legend theme be continuous or discrete?
 #'
 #' @import extrafont
 #' @import ggrepel
 #'
 #' @export
 #'
-set_urbn_defaults <- function(style = "print",
+set_cviog_defaults <- function(style = "print",
                               base_size = 8.5,
                               base_family = "Lato",
                               base_line_size = 0.5,
                               base_rect_size = 0.5,
                               scale = "continuous") {
 
-  # set default theme to theme_urbn_*() --------------------------------------
+  # set default theme to theme_cviog_*() --------------------------------------
 
   if (style == "print") {
 
-    ggplot2::theme_set(theme_urbn_print(base_size = base_size,
+    ggplot2::theme_set(theme_cviog_print(base_size = base_size,
                                         base_family = base_family,
                                         base_line_size = base_line_size,
                                         base_rect_size = base_rect_size))
 
   } else if (style == "map") {
 
-    ggplot2::theme_set(theme_urbn_map(base_size = base_size,
+    ggplot2::theme_set(theme_cviog_map(base_size = base_size,
                                       base_family = base_family,
                                       base_line_size = base_line_size,
                                       base_rect_size = base_rect_size,
@@ -68,22 +68,22 @@ set_urbn_defaults <- function(style = "print",
 
   # set defaults for geoms --------------------------------------------------
 
-  ggplot2::update_geom_defaults("bar", list(fill = "#1696d2"))
-  ggplot2::update_geom_defaults("col", list(fill = "#1696d2"))
-  ggplot2::update_geom_defaults("point", list(colour = "#1696d2", size = 3))
-  ggplot2::update_geom_defaults("line", list(colour = "#1696d2", size = 1))
-  ggplot2::update_geom_defaults("step", list(colour = "#1696d2", size = 1))
-  ggplot2::update_geom_defaults("path", list(colour = "#1696d2", size = 1))
-  ggplot2::update_geom_defaults("boxplot", list(fill = "#1696d2"))
-  ggplot2::update_geom_defaults("density", list(fill = "#1696d2"))
-  ggplot2::update_geom_defaults("violin", list(fill = "#1696d2"))
-  ggplot2::update_geom_defaults("sf", list(fill = "#1696d2", color = "white", size = 0.1))
+  ggplot2::update_geom_defaults("bar", list(fill = "#BA0C2F"))
+  ggplot2::update_geom_defaults("col", list(fill = "#BA0C2F"))
+  ggplot2::update_geom_defaults("point", list(colour = "#BA0C2F", size = 3))
+  ggplot2::update_geom_defaults("line", list(colour = "#BA0C2F", size = 1))
+  ggplot2::update_geom_defaults("step", list(colour = "#BA0C2F", size = 1))
+  ggplot2::update_geom_defaults("path", list(colour = "#BA0C2F", size = 1))
+  ggplot2::update_geom_defaults("boxplot", list(fill = "#BA0C2F"))
+  ggplot2::update_geom_defaults("density", list(fill = "#BA0C2F"))
+  ggplot2::update_geom_defaults("violin", list(fill = "#BA0C2F"))
+  ggplot2::update_geom_defaults("sf", list(fill = "#BA0C2F", color = "#FFFFFF", size = 0.1))
 
   # set defaults for stats --------------------------------------------------
 
-  ggplot2::update_stat_defaults("count", list(fill = "#1696d2"))
-  ggplot2::update_stat_defaults("boxplot", list(fill = "#1696d2"))
-  ggplot2::update_stat_defaults("density", list(fill = "#1696d2"))
-  ggplot2::update_stat_defaults("ydensity", list(fill = "#1696d2"))
+  ggplot2::update_stat_defaults("count", list(fill = "#BA0C2F"))
+  ggplot2::update_stat_defaults("boxplot", list(fill = "#BA0C2F"))
+  ggplot2::update_stat_defaults("density", list(fill = "#BA0C2F"))
+  ggplot2::update_stat_defaults("ydensity", list(fill = "#BA0C2F"))
 
 }
